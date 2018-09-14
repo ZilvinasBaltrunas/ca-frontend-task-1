a0bb9a72c3bc5a1a942b1f2ea363c6fa1eb02a8f
ZilvinasBaltrunas <zilvino@hotmail.com>
2018-09-14 16:34:21 +0300

    final

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Posts from '@/store/Posts'
+import Gallery from '@/store/Gallery'
+export default {
+	modules: {
+		Posts,
+		Gallery
+	},
+	state: {
+		// root state
+	},
+	actions: {
+		// root actions
+	},
+	mutations: {
+		// root mutations
+	},
+	getters: {
+		// root getters
+	}
+}
